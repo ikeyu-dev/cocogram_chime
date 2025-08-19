0951e7596b58f1e0c4d5ee7f66d080ffda3fb003
Ikeda Yuma <141505741+uyuyuu09@users.noreply.github.com>
2025-08-19 17:30:16 +0900

    feat: TODOを取得し，あと何分実施するか表示する機能

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,10 +3,16 @@ import { ref, onUnmounted } from "vue";
 import resultSound from "~/assets/music/chime.mp3";
 
 const currentTime = ref("00:00:00");
+const currentTask = ref("");
+const nextTask = ref("");
+const timeToNextTask = ref("");
+const showColon = ref<boolean>(true);
 const isStarted = ref(false);
 
 let audio: HTMLAudioElement | null = null;
 let clockInterval: number | undefined;
+let taskInterval: number | undefined;
+let timeToNextTaskInterval: number | undefined;
 let chimeInterval: number | undefined;
 
 const setCurrentTime = () => {
@@ -35,6 +41,46 @@ const playChime = () => {
     }
 };
 
+const minuteShedule = [
+    { minute: "00-09", do: "タイピング" },
+    { minute: "10-44", do: "プログラミング" },
+    { minute: "45-49", do: "かたづけ" },
+    { minute: "50-50", do: "あいさつ" },
+    { minute: "51-59", do: "授業準備" },
+];
+
+const getCurrentTask = () => {
+    const currentMinute = Number(
+        currentTime.value.split(":")[1] || currentTime.value.split(" ")[1]
+    );
+    const task = minuteShedule.find((schedule) => {
+        const [start, end] = schedule.minute.split("-").map(Number);
+        return currentMinute >= start && currentMinute <= end;
+    })?.do;
+    currentTask.value = task || "";
+};
+
+const setTimeToNextTask = () => {
+    const currentMinute = Number(
+        currentTime.value.split(":")[1] || currentTime.value.split(" ")[1]
+    );
+    const currentIndex = minuteShedule.findIndex((schedule) => {
+        const [start, end] = schedule.minute.split("-").map(Number);
+        return currentMinute >= start && currentMinute <= end;
+    });
+    const nextIndex =
+        currentIndex === minuteShedule.length - 1 ? 0 : currentIndex + 1;
+    const nextStartMinute = Number(
+        minuteShedule[nextIndex].minute.split("-")[0]
+    );
+
+    let minutesToNextTask = nextStartMinute - currentMinute;
+    if (minutesToNextTask < 0) {
+        minutesToNextTask += 60;
+    }
+    timeToNextTask.value = `あと ${minutesToNextTask} 分`;
+};
+
 const handleStart = () => {
     if (isStarted.value) return;
 
@@ -51,7 +97,11 @@ const handleStart = () => {
     isStarted.value = true;
 
     setCurrentTime();
-    clockInterval = window.setInterval(setCurrentTime, 100);
+    getCurrentTask();
+    setTimeToNextTask();
+    clockInterval = window.setInterval(setCurrentTime, 1000);
+    taskInterval = window.setInterval(getCurrentTask, 1000);
+    timeToNextTaskInterval = window.setInterval(setTimeToNextTask, 1000);
     chimeInterval = window.setInterval(playChime, 100);
 };
 
@@ -78,21 +128,47 @@ onUnmounted(() => {
             </p>
         </div>
 
-        <div
+        <section
             v-else
-            class="bg-white shadow-2xl rounded-2xl p-12 text-center"
+            class="grid grid-cols-1 md:grid-cols-2 gap-2 w-full p-6 bg-white shadow-lg rounded-2xl m-1"
         >
-            <p class="text-2xl md:text-7xl font-semibold text-gray-800 mb-12">
-                現在の時刻
-            </p>
-            <p
-                class="text-4xl md:text-9xl font-extrabold text-gray-900 tracking-wide mb-12"
-            >
-                {{ currentTime }}
-            </p>
-            <p class="text-base md:text-4xl text-gray-500 mt-6 animate-pulse">
-                毎時0/10/45/50分にチャイムが鳴ります
-            </p>
-        </div>
+            <div class="col-span-1 p-12 text-center">
+                <p
+                    class="text-2xl md:text-7xl font-semibold text-gray-800 mb-12"
+                >
+                    現在の時刻
+                </p>
+                <p
+                    class="text-4xl md:text-9xl font-extrabold text-gray-900 tracking-wide mb-12"
+                >
+                    {{ currentTime }}
+                </p>
+                <p
+                    class="text-base md:text-3xl text-gray-500 mt-6 animate-pulse"
+                >
+                    毎時0/10/45/50分にチャイムが鳴ります
+                </p>
+            </div>
+            <div class="col-span-1 p-12 text-center">
+                <p
+                    class="text-2xl md:text-7xl font-semibold text-gray-800 mb-14"
+                >
+                    やること
+                </p>
+                <p
+                    class="text-4xl font-extrabold text-gray-900 tracking-wide mb-12 whitespace-nowrap"
+                    :style="
+                        currentTask.length > 5
+                            ? 'font-size: 5.5rem'
+                            : 'font-size: 6rem'
+                    "
+                >
+                    {{ currentTask }}
+                </p>
+                <p class="text-base md:text-5xl mt-6 animate-pulse">
+                    {{ timeToNextTask }}
+                </p>
+            </div>
+        </section>
     </div>
 </template>

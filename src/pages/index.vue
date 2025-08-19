9d6bc53323ae81ee70e984b27a61f3dd1c5f0150
Ikeda Yuma <141505741+uyuyuu09@users.noreply.github.com>
2025-08-19 17:30:47 +0900

    feat: コロンの表示切替に基づいて時間表示形式を変更

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -16,11 +16,19 @@ let timeToNextTaskInterval: number | undefined;
 let chimeInterval: number | undefined;
 
 const setCurrentTime = () => {
+    if (showColon.value) {
+        showColon.value = false;
+    } else {
+        showColon.value = true;
+    }
     const now = new Date();
     const hours = String(now.getHours()).padStart(2, "0");
     const minutes = String(now.getMinutes()).padStart(2, "0");
-    const seconds = String(now.getSeconds()).padStart(2, "0");
-    currentTime.value = `${hours}:${minutes}:${seconds}`;
+    if (showColon.value) {
+        currentTime.value = `${hours}:${minutes}`;
+    } else {
+        currentTime.value = `${hours} ${minutes}`;
+    }
 };
 
 const playChime = () => {
